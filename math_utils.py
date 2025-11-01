dd2ae6bc50a1f0df68e148bdffd2f577728b0fd2
Waleedshini <walid.shini@live.biu.ac.il>
2025-11-01 19:22:49 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -15,7 +15,7 @@ def find_mean(num1, num2, num3):
     phase1 = (num1+num2+num3)
     phase2 = len(elements)
     result = (phase1/phase2)
-    return result
+    return (result)
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3) 
